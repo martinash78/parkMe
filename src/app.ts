341fa8901c50b99ae8e613914cc0216afacd777a
Martin Ashcroft <martin.ashcroft1978@missguided.com>
2020-05-17 17:13:32 +0100

    Rename index.ts to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import bodyParser from "body-parser";
 const app = express();
-const port = 8080;
-const host = "0.0.0.0";
 import userRoutes from "./routes/user";
 import spaceRoutes from "./routes/space";
 import connectDb from "./connection";
@@ -12,12 +12,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 connectDb().then(() => {
   app.use("/users", userRoutes);
   app.use("/spaces", spaceRoutes);
-
-  app.listen(port, host, () =>
-    console.log(`Example app listening at http://${host}:${port}`)
-  );
-
   function handleError(err: string) {
     return err;
   }
 });
+
+export = app;
